7cd4760ebe1360b2b7b7c3edfcefd2f6d2d102f8
youichiro <cinnamon416@gmail.com>
2018-08-13 12:33:52 +0900

    fix

--- correction.py
+++ correction.py
@@ -5,28 +5,29 @@ from mecab import Mecab
 TARGET_PARTICLES = ['が', 'を', 'に', 'で']
 
 
-def is_missing(current_pos, next_pos, reverse=True):
-    if not reverse:
-        if re.match(r'^(名詞|代名詞|接尾辞-名詞的).*?$', current_pos) \
-           and not re.match(r'^(助詞|助動詞).*?$', next_pos) \
-           and not (current_pos == '名詞-数詞' and next_pos == '名詞-数詞'):
-            return True
-        else:
-            return False
-    elif reverse:
-        if not re.match(r'^(助詞|助動詞).*?$', current_pos) \
-           and re.match(r'^(名詞|代名詞|接尾辞-名詞的).*?$', next_pos) \
-           and not (current_pos == '名詞-数詞' and next_pos == '名詞-数詞'):
-            return True
-        else:
-            return False
-
 class Checker:
     def __init__(self, model_file, mecab_dict_file, reverse=True):
         self.lm = LM(model_file)
         self.mecab = Mecab(mecab_dict_file)
         self.reverse = reverse
+        self.tp = self.tn = self.fp = self.fn = 0
+        self.err, self.res, self.ans = [], [], []
 
+    def is_missing(self, current_pos, next_pos):
+        if not self.reverse:
+            if re.match(r'^(名詞|代名詞|接尾辞-名詞的).*?$', current_pos) \
+            and not re.match(r'^(助詞|助動詞).*?$', next_pos) \
+            and not (current_pos == '名詞-数詞' and next_pos == '名詞-数詞'):
+                return True
+            else:
+                return False
+        elif self.reverse:
+            if not re.match(r'^(助詞|助動詞).*?$', current_pos) \
+            and re.match(r'^(名詞|代名詞|接尾辞-名詞的).*?$', next_pos) \
+            and not (current_pos == '名詞-数詞' and next_pos == '名詞-数詞'):
+                return True
+            else:
+                return False
 
     def best_choice(self, words, idx, choices):
         if self.reverse:
@@ -34,18 +35,50 @@ class Checker:
             idx = len(words) - 1 - idx
         scores = []
         for c in choices:
-            words[idx] = c
-            score = self.lm.probability(words)
+            if c:
+                score = self.lm.probability(words[:idx] + [c] + words[idx+1:])
+            else:
+                score = self.lm.probability(words[:idx] + words[idx+1:])
             scores.append([score, c])
         best_particle = max(scores)[1]
         return best_particle
 
 
-    def correction(self, text):
-        words, parts = self.mecab.tagger(text)
+    def eval_correction(self, idx):
+        if self.err[idx] != self.res[idx]:
+            if self.res[idx] == self.ans[idx]: self.tp += 1
+            elif self.res[idx] != self.ans[idx]: self.fp += 1
+        elif self.err[idx] == self.res[idx]:
+            if self.res[idx] == self.ans[idx]: self.tn += 1
+            elif self.res[idx] != self.ans[idx]: self.fn += 1
+
+
+    def eval_completion(self, idx):
+        if len(self.res) - len(self.err) == 1:
+            if self.res[idx] == self.ans[idx]:
+                self.tp += 1
+            else:
+                self.fp += 1
+                if self.err[idx] == self.ans[idx]:
+                    self.ans = self.ans[:idx] + ['dummy'] + self.ans[idx:]
+        elif len(self.res) - len(self.err) == 0:
+            if self.res[idx] == self.ans[idx]:
+                self.tn += 1
+            else:
+                self.fn += 1
+                self.ans = self.ans[:idx] + self.ans[idx+1:]
+        else:
+            raise ValueError("eval error (completion).")
+    
+
+    def correction(self, err, ans):
+        words, parts = self.mecab.tagger(err)
+        ans_words, _ = self.mecab.tagger(ans)
+        assert len(words) == len(ans_words), '入力文と正解文で単語分割が異なります.'
         if self.reverse:
-            words.reverse()
-            parts.reverse()
+            words = words[::-1]
+            parts = parts[::-1]
+        self.err, self.ans = words[:], ans_words[:]
 
         idx = 0
         while idx < len(parts) - 1:
@@ -53,19 +86,29 @@ class Checker:
             if parts[idx] == '助詞-格助詞' and words[idx] in TARGET_PARTICLES:
                 best_particle = self.best_choice(words, idx, TARGET_PARTICLES)
                 words[idx] = best_particle
+                self.res = words[:]
+                self.eval_correction(idx)
+                self.err = words[:]
 
             # 補完
-            if is_missing(parts[idx], parts[idx + 1], reverse=self.reverse):
+            if self.is_missing(parts[idx], parts[idx + 1]):
                 words.insert(idx+1, 'dummy')
                 parts.insert(idx+1, '助詞-格助詞')
             if words[idx] == 'dummy':
-                best_particle = self.best_choice(words, idx, TARGET_PARTICLES)
-                words[idx] = best_particle
+                best_particle = self.best_choice(words, idx, TARGET_PARTICLES + [''])
+                if best_particle:
+                    words = words[:idx] + [best_particle] + words[idx + 1:]
+                else:
+                    words = words[:idx] + words[idx+1:]
+                    parts = parts[:idx] + parts[idx + 1:]
+                self.res = words[:]
+                self.eval_completion(idx)
+                self.err = words[:]
 
             idx += 1
 
         if self.reverse:
-            words.reverse()
+            words = words[::-1]
         return ''.join(words)
 
 
@@ -82,10 +125,6 @@ def main():
         output = checker.correction(text)
         print(output)
 
-    # text = '彼が車買う'
-    # correct_sent = checker.correction(text)
-    # print(correct_sent)
-
-
+    
 if __name__ == '__main__':
     main()
