90f7359a9ba0099c47a5e9e98918a1fa2a279ea3
youichiro <cinnamon416@gmail.com>
2018-10-11 17:40:00 +0900

    fix

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 import kenlm
 
 class LM:
-    def __init__(self, model_file):
+    def __init__(self, model_file, reverse=True):
         print('Loading KenLM model:', model_file)
         self.lm = kenlm.Model(model_file)
+        self.reverse = reverse
 
     def probability(self, words):
+        if self.reverse:
+            words = words[::-1]
         return self.lm.score(' '.join(words), bos=True, eos=True)
---
+++ test_for_one.py
@@ -0,0 +1,40 @@
+import re
+from calculator import LM
+
+model_file = '/lab/ogawa/tools/kenlm/data/nikkei/nikkei_all_4.binary'
+reverse = True
+test_file = '' # ex) 私 <を> 走る
+TARGET_PARTICLES = ['が', 'を', 'に', 'で']
+lm = LM(model_file, reverse)
+
+
+def lm_choice(text, target_idx):
+    """text: 私 X 走る, target_idx: 2"""
+    scores = []
+    for p in TARGET_PARTICLES:
+        candidate = text[:target_idx] + p + text[target_idx + 1:]
+        score = lm.probability(candidate.split(' '))
+        scores.append([score, p])
+    best = max(score)[1]
+    return best
+
+
+def main():
+    with open(test_file) as f:
+        testdata = f.readlines()
+    count, t = 0, 0
+    for test in testdata:
+        test = test.replace('\n', '')
+        target_idx = test.find('<') + 1
+        ans = test[target_idx]
+        text = test[:target_idx - 1] + 'X' + test[target_idx + 1:]
+        target_idx -= 1
+        predict = lm_choice(text, target_idx)
+        count += 1
+        t += 1 if predict == ans else 0
+    
+    print('Acc. {}% ({}/{})'.format(t / count, t, count))
+
+
+if __name__ == '__main__':
+    main()
