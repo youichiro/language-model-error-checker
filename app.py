227bd6893d869c5bb13740feae66130a1916ee0e
youichiro <cinnamon416@gmail.com>
2018-11-01 22:13:32 +0900

    fix

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import re
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from src.mecab import Mecab
-from src.correction_for_api import Checker
+# from src.correction_for_api import Checker
 
 
 app = Flask(__name__)
@@ -9,35 +9,35 @@ app.config['JSON_AS_ASCII'] = False
 model_file = '/home/ogawa/tools/kenlm_data/nikkei_all_4.binary'
 mecab_dict_file = '/usr/local/lib/mecab/dic/unidic'
 mecab = Mecab(mecab_dict_file)
-checker = Checker(model_file, mecab_dict_file)
+# checker = Checker(model_file, mecab_dict_file)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def top():
-    base_url = 'grammatical-error-checker'
+    base_url = ''
     return render_template('checker.html', base_url=base_url)
-
-
-@app.route('/api/correction', methods=['GET'])
-def correction_api():
-    text = request.args.get('input_text')
-    texts = re.split('[。\n]', text)
-    tokens = []
-    for text in texts:
-        text = text.strip()
-        tokens += checker.correction(text)
-        tokens += [["", 0]]
-    return jsonify(({'tokens': tokens}))
 
 
 # @app.route('/api/correction', methods=['GET'])
 # def correction_api():
 #     text = request.args.get('input_text')
-#     words, _ = mecab.tagger(text)
-#     tokens = [[word, 0] for word in words]
-#     if tokens:
-#         tokens[0][1] = 1
+#     texts = re.split('[。\n]', text)
+#     tokens = []
+#     for text in texts:
+#         text = text.strip()
+#         tokens += checker.correction(text)
+#         tokens += [["", 0]]
 #     return jsonify(({'tokens': tokens}))
+
+
+@app.route('/api/correction', methods=['GET'])
+def correction_api():
+    text = request.args.get('input_text')
+    words, _ = mecab.tagger(text)
+    tokens = [[word, 0] for word in words]
+    if tokens:
+        tokens[0][1] = 1
+    return jsonify(({'tokens': tokens}))
 
 
 if __name__ == '__main__':
--- src/correction_for_api.py
+++ src/correction_for_api.py
@@ -23,49 +23,66 @@ class Checker:
     def best_choice(self, words, idx, choices):
         words = words[::-1]
         idx = len(words) - 1 - idx
-        scores = []
+
+        score = {}
         for c in choices:
             if c:
-                score = self.lm.probability(words[:idx] + [c] + words[idx+1:])
+                score[c] = self.lm.probability(words[:idx] + [c] + words[idx+1:])
             else:
-                score = self.lm.probability(words[:idx] + words[idx+1:])
-            scores.append([score, c])
-        best_particle = max(scores)[1]
-        return best_particle
+                score['none'] = self.lm.probability(words[:idx] + words[idx+1:])
+        best_particle = max(score, key=score.get)
+        best_particle = '' if best_particle == 'none' else best_particle
+        return best_particle, score
+
+        # scores = []
+        # for c in choices:
+        #     if c:
+        #         score = self.lm.probability(words[:idx] + [c] + words[idx+1:])
+        #     else:
+        #         score = self.lm.probability(words[:idx] + words[idx+1:])
+        #     scores.append([score, c])
+        # best_particle = max(scores)[1]
+        # return best_particle
 
     def correction(self, text):
         words, parts = self.mecab.tagger(text)
         words = words[::-1]
         parts = parts[::-1]
         fix_flags = [0] * len(words)
+        scores = [{}] * len(words)
 
         idx = 0
         while idx < len(parts) - 1:
             # 置換
             if parts[idx] == TARGET_POS and words[idx] in TARGET_PARTICLES:
-                best_particle = self.best_choice(words, idx, TARGET_PARTICLES)
+                best_particle, score = self.best_choice(words, idx, TARGET_PARTICLES)
                 if words[idx] != best_particle:
                     words[idx] = best_particle
                     fix_flags[idx] = 1
+                    scores[idx] = score
             # 補完
             if self.is_missing(parts[idx], parts[idx + 1]):
                 words.insert(idx+1, 'dummy')
                 parts.insert(idx+1, TARGET_POS)
                 fix_flags.insert(idx+1, 0)
+                scores.insert(idx+1, 0)
             if words[idx] == 'dummy':
-                best_particle = self.best_choice(words, idx, TARGET_PARTICLES + [''])
+                best_particle, score = self.best_choice(words, idx, TARGET_PARTICLES + [''])
                 if best_particle:
                     words[idx] = best_particle
                     fix_flags[idx] = 1
+                    scores[idx] = score
                 else:
                     words = words[:idx] + words[idx+1:]
                     parts = parts[:idx] + parts[idx+1:]
-                    fix_flags= fix_flags[:idx] + fix_flags[idx+1:]
+                    fix_flags = fix_flags[:idx] + fix_flags[idx+1:]
+                    scores = scores[:idx] + scores[idx+1:]
             idx += 1
 
         words = words[::-1]
         fix_flags = fix_flags[::-1]
-        return [[word, is_fix] for word, is_fix in zip(words, fix_flags)]
+        scores = scores[::-1]
+        return [[word, is_fix, score] for word, is_fix in zip(words, fix_flags, scores)]
 
 
 def test():
