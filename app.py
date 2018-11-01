d53c6a26d7b95fc046330528e12ecb9c9ded4a48
youichiro <cinnamon416@gmail.com>
2018-11-01 17:02:33 +0900

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from src.mecab import Mecab
-# from src.correction_for_api import Checker
+from src.correction_for_api import Checker
 
 
 app = Flask(__name__)
@@ -8,31 +8,31 @@ app.config['JSON_AS_ASCII'] = False
 model_file = '/home/ogawa/tools/kenlm_data/nikkei_all_4.binary'
 mecab_dict_file = '/usr/local/lib/mecab/dic/unidic'
 mecab = Mecab(mecab_dict_file)
-# checker = Checker(model_file, mecab_dict_file)
+checker = Checker(model_file, mecab_dict_file)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def top():
-    base_url = ''
+    base_url = 'grammatical-error-checker'
     return render_template('checker.html', base_url=base_url)
-
-
-# @app.route('/api/correction', methods=['GET'])
-# def correction_api():
-#     text = request.args.get('input_text')
-#     text = text.strip()  # 空白, 改行を削除
-#     tokens = checker.correction(text)
-#     return jsonify(({'tokens': tokens}))
 
 
 @app.route('/api/correction', methods=['GET'])
 def correction_api():
     text = request.args.get('input_text')
-    words, _ = mecab.tagger(text)
-    tokens = [[word, 0] for word in words]
-    if tokens:
-        tokens[0][1] = 1
+    text = text.strip()  # 空白, 改行を削除
+    tokens = checker.correction(text)
     return jsonify(({'tokens': tokens}))
+
+
+# @app.route('/api/correction', methods=['GET'])
+# def correction_api():
+#     text = request.args.get('input_text')
+#     words, _ = mecab.tagger(text)
+#     tokens = [[word, 0] for word in words]
+#     if tokens:
+#         tokens[0][1] = 1
+#     return jsonify(({'tokens': tokens}))
 
 
 if __name__ == '__main__':
