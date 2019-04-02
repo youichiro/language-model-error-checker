28590fc8b383d699c6d1e6e27735edeec7682802
youichiro <cinnamon416@gmail.com>
2019-04-02 23:43:23 +0900

    fix

--- src/test_for_multi.py
+++ src/test_for_multi.py
@@ -34,25 +34,66 @@ def main():
     answer_data = open(args.ans, 'r').readlines()
     reverse = not args.forward
 
-    for i, (err, ans) in enumerate(zip(error_data, answer_data)):
+    acc = 0
+    total_prediction_num = 0
+    acc_of_one = 0
+    n_of_one = 0
+
+    for n, (err, ans) in enumerate(zip(error_data, answer_data)):
         err = err.replace('\n', '')
         ans = ans.replace('\n', '')
         err_words, err_parts = mecab.tagger(err)
         ans_words, ans_parts = mecab.tagger(ans)
         corrected_words =  err_words[::]
 
-        for idx in range(len(err_words) - 1):
-            if err_parts[idx] == TARGET_POS and err_words[idx] in TARGET_PARTICLES:
-                predict = choice(err_words, idx, lm)
-                corrected_words[idx] = predict
+        # 訂正箇所
+        target_idx = [idx for idx in range(len(err_words) - 1)
+                      if err_parts[idx] == TARGET_POS
+                      and err_words[idx] in TARGET_PARTICLES]
+
+        # 文末から訂正
+        if reverse: target_idx[::-1]
+
+        # 訂正箇所が1箇所か複数か
+        is_one_error = True if len(target_idx) else False
+        if is_one_error: n_of_one += 1
+
+        for idx in target_idx:
+            predict = choice(err_words, idx, lm)
+            corrected_words[idx] = predict
+            answer = ans_words[idx]
+
+            if predict == answer:
+                acc += 1
+                if is_one_error:
+                    acc_of_one += 1
+            total_prediction_num += 1
 
         corrected = ''.join(corrected_words)
         if args.show:
-            print(f'{i+1}')
+            print(f'{n+1}')
             print(f'err: {err}')
             print(f'ans: {ans}')
             print(f'out: {corrected}')
-            print(f'Result: {corrected == ans}')
+            print(f'Result: {corrected == ans}\n')
+
+    total_acc = acc / total_prediction_num * 100
+    one_acc = acc_of_one / n_of_one * 100
+    multi_acc = (acc - acc_of_one) / (total_prediction_num - n_of_one) * 100
+    print(f"""
+    \n[Total]
+    Accuracy: {total_acc:.5}%
+    # sentence: {n+1}
+    # total prediction: {total_prediction_num}
+    \n[For one error]
+    Accuracy: {one_acc:.5}%
+    # sentence: {n_of_one}
+    # total prediction: {n_of_one}
+    \n[For multiple error]
+    Accuracy: {multi_acc:.5}%
+    # sentence: {(n+1) - n_of_one}
+    # total prediction: {total_prediction_num - n_of_one}
+    """)
 
 if __name__ == '__main__':
     main()
